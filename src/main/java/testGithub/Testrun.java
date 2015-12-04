d89deb5f58b26260e2665907142ba3faac496a59
Olexandr Myronenko <mironelllo@gmail.com>
2015-12-04 10:59:43 +0200

    testCommit

---
+++ src/main/java/testGithub/Testrun.java
@@ -0,0 +1,10 @@
+package testGithub;
+
+/**
+ * Created by Developer on 04.12.2015.
+ */
+public class Testrun {
+    public static void main(String[] args) {
+        System.out.println("Let's go!");
+    }
+}
